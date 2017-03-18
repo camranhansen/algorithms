3f5e81675afd27b742aafe26dd577b83616b7218
fraggrenadekitten <camran.hansen@ucc.on.ca>
2017-03-18 03:49:30 -0400

    added cayenne, check file for comments about the process.

---
+++ ECOO/Ecoo 2016 Round 1/cayyyyenne.py
@@ -0,0 +1,56 @@
+import math
+'''
+Making your code look good, or even readable? In 2017? what a foreign concept. Sorry about the unreadability of the code, in advance.
+Also, interesting story: I was wondering why my code was not working, (it was returning a value ~2% from test case), and I was starting to get worried. But then I realized that in fact.
+My circle generation function was generating a square. So, takeway: make sure that your functions actually work.
+'''
+def pythag(x1,y1,x2,y2):
+	return math.sqrt(((abs(x1-x2))**2)+((abs(y1-y2))**2))
+def fa(x,y):
+	lns = [100000]*3
+	for pexhs in r:
+		if pythag(x,y,pexhs[0],pexhs[1])<max(lns):
+			lns[lns.index(max(lns))] = pythag(x,y,pexhs[0],pexhs[1])
+	#yes, I know that I do not have to go through this so many times. However, I do not really care much for the effeciency of my solution.. hopefuly?
+	#im going to code a better solution tommorow morning. but right now (3-4AM), not really.
+	lns.sort()
+	# print lns
+	af = [] #the ARRAY of affiliations
+	for b in r:
+		if pythag(x,y,b[0],b[1]) == lns[0] or pythag(x,y,b[0],b[1]) == lns[1] or pythag(x,y,b[0],b[1]) == lns[2]:
+			af.append(b[2])
+	if af.count('D')>=af.count('R'):
+		return 1.0
+	else:
+		return 0.0
+fi = open("DATA/DATA40.txt")
+for i in range(1):
+	z = fi.readline().split(" ")
+	xr = fi.readlines() #make sure that the input actualy works before testing it with an actual test case.
+	r = []
+	count = 0
+	for line in xr:
+		if count <100:
+			r.append(line)
+		count += 1
+	for g in r:
+		h = r.index(g)
+		r[h] = g.split(" ") 
+		#yes I know that list comrephension is a thing as well as the map function
+		#I will probably make this a map function later
+		#but whatever
+		r[h][-1] = r[h][-1][0:1]
+		r[h][0],r[h][1] = int(r[h][0]),int(r[h][1])
+	#strip newlines
+	z[1] = z[1][0:len(z[1])-2]
+	z = map(int,z)
+	# print z
+	democrats = 0.0
+	total = 0.0
+	for xc in range(-50,51,1):
+		for yc in range(-50,51,1):
+			if pythag(xc,yc,0,0) <=50.0:
+				democrats += fa((xc+z[0]),(yc+z[1]))
+				total += 1.0
+	print (round((democrats/total),3))*100
+fi.close()
