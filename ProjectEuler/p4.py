3cd53688d43e3cd3731b8f062c0641f0c428a28f
fraggrenadekitten <camran.hansen@ucc.on.ca>
2017-03-24 01:49:42 -0400

    I'm starting to get a hang of these problems.

--- ProjectEuler/p4.py
+++ ProjectEuler/p4.py
@@ -1,7 +1,15 @@
 '''
 A palindromic number reads the same both ways. 
-The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 -------------------------------------------------------------------------
+I know I will get some square overlap in my method of doing this, but whatever.
+Im not trying to make the most effecient solutions - just trying to solve these relatively noncomplex problems so that I can get to the more interesting ones
 '''
+n = []
+for x in range(999,0,-1):
+	for y in range(999,0,-1):
+		if str(x*y) == str(x*y)[::-1]:
+			n.append(x*y)
+print max(n)
